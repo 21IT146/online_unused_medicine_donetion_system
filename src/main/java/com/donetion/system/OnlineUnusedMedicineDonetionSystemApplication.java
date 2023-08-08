1349d91206375401c955f1cc65d57dd161ea0429
Nasir Sherasiya <21it146@charusat.edu.in>
2023-08-09 00:08:40 +0530

    first commit

---
+++ src/main/java/com/donetion/system/OnlineUnusedMedicineDonetionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.donetion.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineUnusedMedicineDonetionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineUnusedMedicineDonetionSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/donetion/system/OnlineUnusedMedicineDonetionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donetion.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineUnusedMedicineDonetionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
